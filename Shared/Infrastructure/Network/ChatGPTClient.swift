7b6024e23830bba8fb74dea456682b5673aabf49
Shota Abe <abesyouta516@gmail.com>
2023-03-09 14:46:19 +0900

    ChatGPTClientを実装

---
+++ Shared/Infrastructure/Network/ChatGPTClient.swift
@@ -0,0 +1,24 @@
+//
+//  ChatGPTClient.swift
+//  ChatGPTForXcodeEditorExtension
+//
+//  Created by 安部翔太 on 2023/03/09.
+//
+
+import OpenAISwift
+
+struct ChatGPTClient {
+    func send(
+        authToken: String,
+        chatMessages: [ChatMessage]
+    ) async throws -> String {
+        let openAI = OpenAISwift(authToken: authToken)
+        let result = try await openAI.sendChat(
+            with: chatMessages,
+            model: .chat(.chatgpt),
+            maxTokens: 4096
+        )
+        
+        return result.choices.first?.message.content ?? ""
+    }
+}
