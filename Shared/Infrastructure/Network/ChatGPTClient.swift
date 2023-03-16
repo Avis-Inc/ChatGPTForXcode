505d6e5f157b4cf422497f62e1eeb37ccded48ef
SNQ-2001 <taishin05109@gmail.com>
2023-03-16 17:29:53 +0900

    Added Combine and Closure API clients for ChatGPTClient

--- Shared/Infrastructure/Network/ChatGPTClient.swift
+++ Shared/Infrastructure/Network/ChatGPTClient.swift
@@ -5,9 +5,11 @@
 //  Created by 安部翔太 on 2023/03/09.
 //
 
+import Combine
 import OpenAISwift
 
 struct ChatGPTClient {
+    // MARK: Swift Concurrency
     static func send(
         authToken: String,
         chatMessages: [ChatMessage]
@@ -18,7 +20,44 @@ struct ChatGPTClient {
             model: .chat(.chatgpt),
             maxTokens: 4096
         )
-        
+
         return result.choices.first?.message.content ?? ""
+    }
+
+    // MARK: Combine
+    static func send(
+        authToken: String,
+        chatMessages: [ChatMessage]
+    ) -> AnyPublisher<String, OpenAIError> {
+        Future { promise in
+            let openAI = OpenAISwift(authToken: authToken)
+            openAI.sendChat(
+                with: chatMessages,
+                model: .chat(.chatgpt),
+                maxTokens: 4096
+            ) { result in
+                switch result {
+                case let .success(message):
+                    promise(.success(message.choices.first?.message.content ?? ""))
+                case let .failure(error):
+                    promise(.failure(error))
+                }
+            }
+        }.eraseToAnyPublisher()
+    }
+
+    // MARK: Closure
+    static func send(
+        authToken: String,
+        chatMessages: [ChatMessage],
+        completionHandler: @escaping (Result<OpenAI<MessageResult>, OpenAIError>) -> Void
+    ) {
+        let openAI = OpenAISwift(authToken: authToken)
+        openAI.sendChat(
+            with: chatMessages,
+            model: .chat(.chatgpt),
+            maxTokens: 4096,
+            completionHandler: completionHandler
+        )
     }
 }
