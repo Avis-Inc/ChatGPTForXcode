08948540fd7a9469d8d9eee073a0963104365f58
Shota Abe <abesyouta516@gmail.com>
2023-03-23 21:22:25 +0900

    プロンプトの精度を改善

--- Shared/Domain/Entity/Prompt.swift
+++ Shared/Domain/Entity/Prompt.swift
@@ -12,11 +12,11 @@ public enum Prompt {
     static func review(_ code: String, language: Language) -> String {
         """
         # decree: 
-        Please output the problems with the input code. However, the following constraints must be met.
+        Please output the problems with the input code. However, it is absolutely essential that the following constraints must be met.
         
         # constraint: 
         - If anything is unclear, please do not list anything.
-        - If there is no problem, just output "LGTM
+        - If there is no problem, just output one word: "LGTM".
         - If there are any problems, please specify the specific areas.
         - Please keep sentences as short as possible and write a maximum of three items in list form.
         - \(language.prompt)
